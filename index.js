407ce7505e4adaa3e6b275fb510d4f576216e570
Thomas Edwards <thce28@gmail.com>
2019-02-04 10:00:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-function takeANumber(KatzDeliLine, name) {
+// function takeANumber(KatzDeliLine, name) {
   
-  KatzDeliLine.push(name);   // adds name to end of line array
+//   KatzDeliLine.push(name);   // adds name to end of line array
   
-  var length = KatzDeliLine.length;
-  return "Welcome, " + name + ". You are number " + length.toString() + " in line.";  
+//   var length = KatzDeliLine.length;
+//   return "Welcome, " + name + ". You are number " + length.toString() + " in line.";  
+// }
+
+var count = 0;
+
+function takeANumber(arr) {
+  
+  count += 1;
+  
+  arr.push(count);
+  
+  return "Welcome, you are number: " + count
 }
+
+
 
 function nowServing(KatzDeliLine) {
   if (KatzDeliLine.length < 1) {
